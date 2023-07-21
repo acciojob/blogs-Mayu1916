cb6acad927e172ae88bb4b0917135cb6491e10f4
Mayu1916 <69160936+Mayu1916@users.noreply.github.com>
2023-07-21 19:18:16 +0530

    test

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -7,8 +7,10 @@ import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.auditing.CurrentDateTimeProvider;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -23,11 +25,15 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        User user =userRepository1.findById(userId).get();
+        Blog blog=new Blog(title,content,user);
+        blog.setPubDate(new Date());
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
-        //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,38 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog= blogRepository2.findById(blogId).get();
+        Image image= new Image(description,dimensions,blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        String arr[] = screenDimensions.split("X");
+        Integer x=Integer.parseInt(arr[0]);
+        Integer y=Integer.parseInt(arr[1]);
 
+        Image image=imageRepository2.findById(id).get();
+        String imgeScr[]=image.getDimensions().split("X");
+
+
+        int x1=Integer.parseInt(imgeScr[0]);
+        int y1=Integer.parseInt(imgeScr[1]);
+
+        int totalSizeOfImage=x1*y1;
+        int screenSize=x*y;
+
+
+        int firstDim=x/x1;
+        int secondDim=y/y1;
+        int totalImageCanBeFit=firstDim*secondDim;
+
+        return totalImageCanBeFit;
     }
 }
